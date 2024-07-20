f8479efdba800946ca3f47daf61f7ef7d9d109d0
Aaquib974787 <83481277+Aaquib974787@users.noreply.github.com>
2024-07-20 15:36:49 +0530

    Delete Portfolio directory

--- Portfolio/script.js
+++
@@ -1,8 +0,0 @@
-// <!-- it is basically typing animation liabray of javascript -->
-<script src="https://unpkg.com/typed.js@2.1.0/dist/typed.umd.js"></script>
-
-    var typed = new Typed('#element', {
-      strings: ['Data Analytics','Web developer','Power Bi','Programming (C,Python)','MIS Activity','Software Development'],
-      typeSpeed: 50,
-    });
-  
